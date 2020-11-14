49cdebee49409c0aad23c22531ad1f1ed9969a61
Patxi Bocos <patxi952@gmail.com>
2020-11-14 11:03:10 +0100

    Chapter 11 Programmers and Binary Tree exercises

--- src/Chapter11.hs
+++ src/Chapter11.hs
@@ -50,3 +50,71 @@ instance TooMany (Int, Int) where
 
 instance (Num a, TooMany a) => TooMany (a, a) where
   tooMany (a, b) = tooMany (a + b)
+
+-- Exercise: Programmers
+
+data OperatingSystem
+  = GnuPlusLinux
+  | OpenBSDPlusNevermindJustBSDStill
+  | Mac
+  | Windows
+  deriving (Eq, Show)
+
+data ProgLang
+  = Haskell
+  | Agda
+  | Idris
+  | PureScript
+  deriving (Eq, Show)
+
+data Programmer = Programmer
+  { os :: OperatingSystem,
+    lang :: ProgLang
+  }
+  deriving (Eq, Show)
+
+allOperatingSystems :: [OperatingSystem]
+allOperatingSystems =
+  [ GnuPlusLinux,
+    OpenBSDPlusNevermindJustBSDStill,
+    Mac,
+    Windows
+  ]
+
+allLanguages :: [ProgLang]
+allLanguages =
+  [ Haskell,
+    Agda,
+    Idris,
+    PureScript
+  ]
+
+allProgrammers :: [Programmer]
+allProgrammers = [Programmer {os = o, lang = l} | o <- allOperatingSystems, l <- allLanguages]
+
+-- Exercises: Binary Tree
+
+data BinaryTree a
+  = Leaf
+  | Node (BinaryTree a) a (BinaryTree a)
+  deriving (Eq, Ord, Show)
+
+mapTree :: (a -> b) -> BinaryTree a -> BinaryTree b
+mapTree _ Leaf = Leaf
+mapTree f (Node left a right) = Node (mapTree f left) (f a) (mapTree f right)
+
+preorder :: BinaryTree a -> [a]
+preorder Leaf = []
+preorder (Node left x right) = x : preorder left ++ preorder right
+
+inorder :: BinaryTree a -> [a]
+inorder Leaf = []
+inorder (Node left x right) = inorder left ++ x : inorder right
+
+postorder :: BinaryTree a -> [a]
+postorder Leaf = []
+postorder (Node left x right) = (postorder left ++ postorder right) ++ [x]
+
+foldTree :: (a -> b -> b) -> b -> BinaryTree a -> b
+foldTree _ acc Leaf = acc
+foldTree f acc (Node left x right) = foldTree f (f x (foldTree f acc left)) right
