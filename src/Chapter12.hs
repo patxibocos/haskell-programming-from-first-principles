e88669450266c9a4d7f914a017512011b12f5f34
Patxi Bocos <patxi952@gmail.com>
2020-11-24 20:39:19 +0100

    Finishing Chapter 12 exercises 🎉

--- src/Chapter12.hs
+++ src/Chapter12.hs
@@ -134,3 +134,31 @@ either' _ bToC (Right b) = bToC b
 
 eitherMaybe'' :: (b -> c) -> Either a b -> Maybe c
 eitherMaybe'' f = either' (const Nothing) (Just . f)
+
+-- Write your own iterate and unfoldr
+
+myIterate :: (a -> a) -> a -> [a]
+myIterate f a = a : myIterate f (f a)
+
+myUnfoldr :: (b -> Maybe (a, b)) -> b -> [a]
+myUnfoldr f b = case f b of
+  Nothing -> []
+  (Just (a, b)) -> a : myUnfoldr f b
+
+betterIterate :: (a -> a) -> a -> [a]
+betterIterate f = myUnfoldr (\a -> Just (a, f a))
+
+-- Finally something other than a list!
+
+data BinaryTree a
+  = Leaf
+  | Node (BinaryTree a) a (BinaryTree a)
+  deriving (Eq, Ord, Show)
+
+unfold :: (a -> Maybe (a, b, a)) -> a -> BinaryTree b
+unfold f a = case f a of
+  Nothing -> Leaf
+  (Just (a1, b, a2)) -> Node (unfold f a1) b (unfold f a2)
+
+treeBuild :: Integer -> BinaryTree Integer
+treeBuild n = unfold (\a -> if a <= 0 then Nothing else Just (a - 1, n - a, a - 1)) n
