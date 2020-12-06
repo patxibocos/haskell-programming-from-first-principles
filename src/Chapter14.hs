ddec0c98769871999d683683cc8f51a58eeb3bd6
Patxi Bocos <patxi952@gmail.com>
2020-12-06 18:11:28 +0100

    More property tests

--- src/Chapter14.hs
+++ src/Chapter14.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Chapter14 where
 
+import Chapter11 (capitalizeWord)
 import Chapter8 (digitToWord, digits, wordNumber)
 import Data.List
 import Test.Hspec
@@ -37,8 +40,8 @@ half x = x / 2
 halfIdentity :: Double -> Double
 halfIdentity = (* 2) . half
 
-halfIdentityProp :: Property
-halfIdentityProp = forAll (arbitrary :: Gen Double) (\x -> halfIdentity x == x)
+prop_halfIdentity :: Property
+prop_halfIdentity = forAll (arbitrary :: Gen Double) (\x -> halfIdentity x == x)
 
 listOrdered :: (Ord a) => [a] -> Bool
 listOrdered xs =
@@ -48,5 +51,119 @@ listOrdered xs =
     go y (Nothing, t) = (Just y, t)
     go y (Just x, t) = (Just y, x >= y)
 
-listOrderedProp :: Property
-listOrderedProp = forAll (arbitrary :: Gen [Int]) (listOrdered . sort)
+prop_listOrdered :: Property
+prop_listOrdered = forAll (arbitrary :: Gen [Int]) (listOrdered . sort)
+
+plusAssociative :: Int -> Int -> Int -> Bool
+plusAssociative x y z = x + (y + z) == (x + y) + z
+
+prop_plusAssociative :: Property
+prop_plusAssociative = forAll (arbitrary :: Gen (Int, Int, Int)) (\(x, y, z) -> plusAssociative x y z)
+
+plusCommutative :: Int -> Int -> Bool
+plusCommutative x y = x + y == y + x
+
+prop_plusCommutative :: Property
+prop_plusCommutative = forAll (arbitrary :: Gen (Int, Int)) (uncurry plusCommutative)
+
+multAssociative :: Int -> Int -> Int -> Bool
+multAssociative x y z = x * (y * z) == (x * y) * z
+
+prop_multAssociative :: Property
+prop_multAssociative = forAll (arbitrary :: Gen (Int, Int, Int)) (\(x, y, z) -> multAssociative x y z)
+
+multCommutative :: Int -> Int -> Bool
+multCommutative x y = x * y == y * x
+
+prop_multCommutative :: Property
+prop_multCommutative = forAll (arbitrary :: Gen (Int, Int)) (uncurry multCommutative)
+
+nonZeroInts :: Gen (Int, Int)
+nonZeroInts = do
+  x <- arbitrary `suchThat` (/= 0)
+  y <- arbitrary `suchThat` (/= 0)
+  return (x, y)
+
+prop_quotRem :: Property
+prop_quotRem = forAll nonZeroInts (\(x, y) -> quot x y * y + rem x y == x)
+
+prop_divMod :: Property
+prop_divMod = forAll nonZeroInts (\(x, y) -> div x y * y + mod x y == x)
+
+twoPositiveInts :: Gen (Int, Int)
+twoPositiveInts = do
+  x <- arbitrary `suchThat` (>= 0)
+  y <- arbitrary `suchThat` (>= 0)
+  return (x, y)
+
+threePositiveInts :: Gen (Int, Int, Int)
+threePositiveInts = do
+  (x, y) <- twoPositiveInts
+  z <- arbitrary `suchThat` (>= 0)
+  return (x, y, z)
+
+-- (^) is not associative
+prop_expNOTAssociative :: Property
+prop_expNOTAssociative = forAll threePositiveInts (\(x, y, z) -> x ^ (y ^ z) == (x ^ y) ^ z)
+
+-- (^) is not commutative
+prop_expNOTCommutative :: Property
+prop_expNOTCommutative = forAll twoPositiveInts (\(x, y) -> x ^ y == y ^ x)
+
+prop_reverseList :: Property
+prop_reverseList = forAll (arbitrary :: Gen [Int]) (\xs -> (reverse . reverse) xs == id xs)
+
+prop_dollar :: Property
+prop_dollar = forAll (arbitrary :: Gen Int) (\x -> id $ x == id x)
+
+prop_compose :: Property
+prop_compose = forAll (arbitrary :: Gen Int) (\x -> (id . id) x == id (id x))
+
+twoLists :: Gen ([Int], [Int])
+twoLists = do
+  xs <- arbitrary
+  ys <- arbitrary
+  return (xs, ys)
+
+prop_foldrConsConcatenation :: Property
+prop_foldrConsConcatenation = forAll twoLists (\(xs, ys) -> foldr (:) xs ys == ys ++ xs)
+
+prop_foldrConcatenationConcat :: Property
+prop_foldrConcatenationConcat = forAll (arbitrary :: Gen [[Int]]) (\x -> foldr (++) [] x == concat x)
+
+intAndList :: Gen ([Int], Int)
+intAndList = do
+  xs <- arbitrary
+  n <- arbitrary
+  return (xs, n)
+
+prop_TakeThenLengthIsNOTn :: Property
+prop_TakeThenLengthIsNOTn = forAll intAndList (\(xs, n) -> length (take n xs) == n)
+
+prop_roundTrip :: Property
+prop_roundTrip = forAll (arbitrary :: Gen String) (\x -> read (show x) == x)
+
+-- Idempotence
+
+twice :: (b -> b) -> b -> b
+twice f = f . f
+
+fourTimes :: (b -> b) -> b -> b
+fourTimes = twice . twice
+
+capitalizeIdempotent :: String -> Bool
+capitalizeIdempotent x = (capitalizeWord x == twice capitalizeWord x) && (capitalizeWord x == fourTimes capitalizeWord x)
+
+sortIdempotent :: Ord a => [a] -> Bool
+sortIdempotent x = (sort x == twice sort x) && (sort x == fourTimes sort x)
+
+prop_capitalizeIdempotent :: Property
+prop_capitalizeIdempotent = forAll (arbitrary :: Gen String) capitalizeIdempotent
+
+prop_sortIdempotent :: Property
+prop_sortIdempotent = forAll (arbitrary :: Gen [Int]) sortIdempotent
+
+return []
+
+runTests :: IO Bool
+runTests = $quickCheckAll
