df7272eeee5398bef13e6eb9a4f15701c9941bff
Patxi Bocos <patxi952@gmail.com>
2020-12-29 21:30:45 +0100

    Chapter 20 exercises finished 🎉

--- src/Chapter20.hs
+++ src/Chapter20.hs
@@ -1,6 +1,11 @@
 module Chapter20 where
 
+import Chapter15 (Three (Three), Two (Two))
+import Chapter16 (Three' (Three'))
 import Data.Monoid
+import Test.QuickCheck.Arbitrary
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
 
 -- Exercises: Library Functions
 
@@ -38,3 +43,43 @@ fold = foldMap id
 
 foldMap' :: (Foldable t, Monoid m) => (a -> m) -> t a -> m
 foldMap' f = foldr (\a b -> f a <> b) mempty
+
+-- 20.6 Chapter Exercises
+
+data Constant a b = Constant b deriving (Show)
+
+instance Foldable (Constant a) where
+  foldMap f (Constant b) = f b
+
+instance (Arbitrary b) => Arbitrary (Constant a b) where
+  arbitrary = Constant <$> arbitrary
+
+instance Foldable (Two a) where
+  foldMap f (Two _ b) = f b
+
+instance Foldable (Three a b) where
+  foldMap f (Three _ _ c) = f c
+
+instance Foldable (Three' a) where
+  foldMap f (Three' _ b1 b2) = f b1 <> f b2
+
+data Four' a b = Four' a b b b deriving (Show)
+
+instance Foldable (Four' a) where
+  foldMap f (Four' _ b1 b2 b3) = f b1 <> f b2 <> f b3
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Four' a b) where
+  arbitrary = Four' <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+
+type Quintuple = (Int, Double, String, Rational, Integer)
+
+filterF :: (Applicative f, Foldable t, Monoid (f a)) => (a -> Bool) -> t a -> f a
+filterF f = foldMap (\a -> if f a then pure a else mempty)
+
+runTests :: IO ()
+runTests = do
+  quickBatch $ foldable (undefined :: (Constant Int Quintuple))
+  quickBatch $ foldable (undefined :: (Two Int Quintuple))
+  quickBatch $ foldable (undefined :: (Three Int String Quintuple))
+  quickBatch $ foldable (undefined :: (Three' Int Quintuple))
+  quickBatch $ foldable (undefined :: (Four' Int Quintuple))
