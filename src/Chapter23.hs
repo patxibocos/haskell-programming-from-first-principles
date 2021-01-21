9507f92760a1b28d3e91e55542dfcdfd9c4aecbe
Patxi Bocos <patxi952@gmail.com>
2021-01-21 14:19:41 +0100

    Moi (State) Applicative and Monad

--- src/Chapter23.hs
+++ src/Chapter23.hs
@@ -45,8 +45,32 @@ rollsCountLogged n = go 0 []
         let (die, nextGen) = randomR (1, 6) gen
          in go (sum + die) (dies ++ [intToDie die]) nextGen
 
+-- 23.6 Write State for yourself
+
 newtype Moi s a = Moi {runMoi :: s -> (a, s)}
 
 instance Functor (Moi s) where
   fmap :: (a -> b) -> Moi s a -> Moi s b
   fmap f (Moi g) = Moi {runMoi = \s -> let (a1, s1) = g s in (f a1, s1)}
+
+instance Applicative (Moi s) where
+  pure :: a -> Moi s a
+  pure a = Moi {runMoi = \s -> (a, s)}
+  (<*>) :: Moi s (a -> b) -> Moi s a -> Moi s b
+  (Moi f) <*> (Moi g) =
+    Moi
+      { runMoi = \s ->
+          let (fa, s1) = f s
+              (a, s2) = g s1
+           in (fa a, s2)
+      }
+
+instance Monad (Moi s) where
+  return = pure
+  (>>=) :: Moi s a -> (a -> Moi s b) -> Moi s b
+  (Moi f) >>= g =
+    Moi
+      { runMoi = \s ->
+          let (a, s1) = f s
+           in runMoi (g a) s1
+      }
